6a863b9226be830e0c1853018d84aaf5994ec4d8
Anoskie <anoskie551@gmail.com>
2024-09-20 12:45:47 +0300

    Add files via upload

---
+++ do while.cpp
@@ -0,0 +1,16 @@
+// do while loop
+#include <iostream>
+
+int main () {
+
+int count = 1;
+do {
+   std::cout<<"hello there"<<std::endl;
+   }
+   while (count <= 10); {
+   
+   count = count + 1;
+   }
+   return 0;
+   }
+
