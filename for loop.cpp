6431e2e80370496dec35eb84cade2e20be192809
Anoskie <anoskie551@gmail.com>
2024-09-20 12:46:12 +0300

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,13 @@
+//for loop
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int count = 1; count<=10; count++)
+  
+  {
+   std::cout<<"hello there"<< std::endl;
+    }
+    
+    return 0;
+    }
